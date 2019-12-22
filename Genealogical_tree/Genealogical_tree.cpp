4f5984a22f514070aa4afa7efdd9d7ebb0ea3677
Delor Rotstrump <delor4@gmail.com>
2019-12-22 16:05:28 +0100

    main switch -> std::unordered_map

--- Genealogical_tree/Genealogical_tree.cpp
+++ Genealogical_tree/Genealogical_tree.cpp
@@ -102,39 +102,6 @@ Person edit_person(GenTreeItem* i)
 
     return Person(ofn, oln, by, sx[0]);
 }
-void save_tree(GenTree& tree)
-{
-    intro();
-    std::cout << "Zapisywanie drzewa do pliku.\n\n";
-    std::cout << "Podaj nazwe pliku:\n";
-    std::string path = get_string("");
-    if (!path.length())
-        return;
-
-    tree.save(path);
-}
-void load_tree(GenTree& tree)
-{
-    intro();
-    std::cout << "Wczytywanie drzewa z pliku.\n\n";
-    std::cout << "Podaj nazwe pliku:\n";
-    std::string path = get_string("");
-    if (!path.length())
-        return;
-
-    GenTree n_tree;
-    if (n_tree.load(path)) {
-        tree.swap(n_tree);
-    };
-}
-void show_version()
-{
-    intro();
-    std::cout << "\n";
-    std::cout << "by Sebastian Kucharczyk, 2019\n\n";
-    std::cout << "Ver: 1.0\n\n";
-    std::cout << "Build: " __TIME__ " " __DATE__ "\n\n";
-}
 void show_persons_list(std::vector<Person>& l, std::string title){
     if(l.size()){
         std::cout << title << "\n";
@@ -160,6 +127,140 @@ void show_info(GenTreeItem *p)
     p->get_grandchildrens(grandchildrens);
     show_persons_list(grandchildrens, "\nWnuki:");
 }
+void on_exit(GenTree &, GT_Menu &, Console &)
+{
+    exit(0);
+}
+void on_add_person(GenTree & tree, GT_Menu &menu, Console &console)
+{
+    console.cls();
+    Person p = input_new_person();
+    tree.add_person(tree.find_by_id(menu.get_curr_line()), p);
+}
+void on_edit_person(GenTree & tree, GT_Menu &menu, Console &console)
+{
+    console.cls();
+    auto i = tree.find_by_id(menu.get_curr_line());
+    if (i) {
+        auto p = edit_person(i);
+        tree.set_person(i, p);
+    }
+}
+void on_delete_person(GenTree & tree, GT_Menu &menu, Console &console)
+{
+    tree.remove_by_id(menu.get_curr_line());
+}
+void on_show_info(GenTree & tree, GT_Menu &menu, Console &console)
+{
+    console.cls();
+    auto i = tree.find_by_id(menu.get_curr_line());
+    if(i){
+        show_info(i);
+        console.get_key();
+    }
+}
+void on_load(GenTree & tree, GT_Menu &menu, Console &console)
+{
+    console.cls();
+    intro();
+
+    std::cout << "Wczytywanie drzewa z pliku.\n\n";
+    std::cout << "Podaj nazwe pliku:\n";
+    std::string path = get_string("");
+    if (!path.length())
+        return;
+
+    GenTree n_tree;
+    if (n_tree.load(path)) {
+        tree.swap(n_tree);
+    };
+}
+void on_save(GenTree & tree, GT_Menu &menu, Console &console)
+{
+    if (tree.get_size()) {
+        console.cls();
+        intro();
+        std::cout << "Zapisywanie drzewa do pliku.\n\n";
+        std::cout << "Podaj nazwe pliku:\n";
+        std::string path = get_string("");
+        if (!path.length())
+            return;
+
+        tree.save(path);
+    }
+}
+void on_version(GenTree & tree, GT_Menu &menu, Console &console)
+{
+    console.cls();
+
+    intro();
+    std::cout << "\n";
+    std::cout << "by Sebastian Kucharczyk, 2019\n\n";
+    std::cout << "Ver: 1.0\n\n";
+    std::cout << "Build: " __TIME__ " " __DATE__ "\n\n";
+
+    console.get_key();
+}
+void on_new_tree(GenTree & tree, GT_Menu &menu, Console &console)
+{
+    tree.remove_by_id(0);
+}
+void on_arrow_up(GenTree & tree, GT_Menu &menu, Console &console)
+{
+    menu.line_up();
+}
+void on_arrow_down(GenTree & tree, GT_Menu &menu, Console &console)
+{
+    menu.line_down();
+}
+void on_arrow_left(GenTree & tree, GT_Menu &menu, Console &console)
+{
+    menu.set_curr_line(tree.get_id(tree.find_by_id(menu.get_curr_line())->get_parent()));
+}
+void on_arrow_right(GenTree & tree, GT_Menu &menu, Console &console)
+{
+    auto c = tree.find_by_id(menu.get_curr_line())->get_leftmost_child();
+    if (c)
+        menu.set_curr_line(tree.get_id(c));
+}
+void on_page_up(GenTree & tree, GT_Menu &menu, Console &console)
+{
+    menu.set_curr_line(menu.get_curr_line() - menu.get_max_lines() + 1);
+}
+void on_page_down(GenTree & tree, GT_Menu &menu, Console &console)
+{
+    menu.set_curr_line(menu.get_curr_line() + menu.get_max_lines() - 1);
+}
+
+std::unordered_map<GT_Menu::MENU_ITEMS, void (*)(GenTree &, GT_Menu &, Console &), std::hash<int>> get_main_loop()
+{
+    std::unordered_map<GT_Menu::MENU_ITEMS, void (*)(GenTree &, GT_Menu &, Console &), std::hash<int>> loop_map;
+
+    loop_map[GT_Menu::EXIT] = on_exit;
+    loop_map[GT_Menu::ADD_PERSON] = on_add_person;
+    loop_map[GT_Menu::EDIT_PERSON] = on_edit_person;
+    loop_map[GT_Menu::DELETE_PERSON] = on_delete_person;
+
+    loop_map[GT_Menu::SHOW_INFO] = on_show_info;
+
+    loop_map[GT_Menu::LOAD] = on_load;
+    loop_map[GT_Menu::SAVE] = on_save;
+
+    loop_map[GT_Menu::NEW_TREE] = on_new_tree;
+    loop_map[GT_Menu::ARROW_UP] = on_arrow_up;
+    loop_map[GT_Menu::ARROW_DOWN] = on_arrow_down;
+    loop_map[GT_Menu::ARROW_LEFT] = on_arrow_left;
+    loop_map[GT_Menu::ARROW_RIGHT] = on_arrow_right;
+
+    loop_map[GT_Menu::PAGE_UP] = on_page_up;
+    loop_map[GT_Menu::PAGE_DOWN] = on_page_down;
+
+    loop_map[GT_Menu::EXIT] = on_exit;
+
+    loop_map[GT_Menu::VERSION] = on_version;
+
+    return loop_map;
+}
 int main()
 {
     Console console;
@@ -170,6 +271,7 @@ int main()
     init_tree(tree);
 #endif // __DEBUG
 
+    auto loop = get_main_loop();
     while (true) {
         menu.set_curr_max_lines(tree.get_size());
         console.cls();
@@ -185,75 +287,11 @@ int main()
         while (opt == GT_Menu::NONE)
             opt = menu.get_option(console.get_key());
 
-        switch (opt) {
-        case GT_Menu::EXIT:
-            exit(0);
-        case GT_Menu::ADD_PERSON: {
-            console.cls();
-            Person p = input_new_person();
-            tree.add_person(tree.find_by_id(menu.get_curr_line()), p);
-        } break;
-        case GT_Menu::EDIT_PERSON: {
-            console.cls();
-            auto i = tree.find_by_id(menu.get_curr_line());
-            if (i) {
-                auto p = edit_person(i);
-                tree.set_person(i, p);
-            }
-        } break;
-        case GT_Menu::SHOW_INFO: {
-            console.cls();
-            auto i = tree.find_by_id(menu.get_curr_line());
-            if(i){
-                show_info(i);
-                console.get_key();
-            }
-        } break;
-        case GT_Menu::DELETE_PERSON:
-            tree.remove_by_id(menu.get_curr_line());
-            break;
-        case GT_Menu::NEW_TREE:
-            tree.remove_by_id(0);
-            break;
-        case GT_Menu::LOAD:
-            console.cls();
-            load_tree(tree);
-            break;
-        case GT_Menu::SAVE:
-            if (tree.get_size()) {
-                console.cls();
-                save_tree(tree);
-            }
-            break;
-        case GT_Menu::ARROW_DOWN:
-            menu.line_down();
-            break;
-        case GT_Menu::ARROW_UP:
-            menu.line_up();
-            break;
-        case GT_Menu::ARROW_LEFT:
-            menu.set_curr_line(tree.get_id(tree.find_by_id(menu.get_curr_line())->get_parent()));
-            break;
-        case GT_Menu::ARROW_RIGHT: {
-            auto c = tree.find_by_id(menu.get_curr_line())->get_leftmost_child();
-            if (c)
-                menu.set_curr_line(tree.get_id(c));
-        } break;
-        case GT_Menu::PAGE_UP:
-            menu.set_curr_line(menu.get_curr_line() - menu.get_max_lines() + 1);
-            break;
-        case GT_Menu::PAGE_DOWN:
-            menu.set_curr_line(menu.get_curr_line() + menu.get_max_lines() - 1);
-            break;
-        case GT_Menu::VERSION:
-            console.cls();
-            show_version();
-            console.get_key();
-            break;
-        default:
-            cout << opt;
-            break;
-        };
+        if(loop.find(opt) != loop.end()){
+            loop.at(opt)(tree, menu, console);
+        } else {
+            cout << "[ERROR] Nieznana opcja: " << opt;
+        }
     }
     return 0;
 }
